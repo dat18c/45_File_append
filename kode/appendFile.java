04156c7c25c7d9ca28af8192a6f8395edab0a1ec
JarlTuxen <jart@kea.dk>
2018-11-12 09:11:55 +0100

    try catch

--- kode/appendFile.java
+++ kode/appendFile.java
@@ -3,21 +3,30 @@ import java.io.*;
 public class appendFile
 {
   public static void main(String[] args)
+   // throws IOException
   {
    try
    {
-      //lav filobjekt
-      FileWriter fw = new FileWriter("minFil.txt", true);
+      //lav fileWriter-objekt med append
+      FileWriter fw = new FileWriter("Fejl", true);
       //lav buffer, da der ellers bliver skrevet med det samme.
       //Det kan være dyrt i forhold til at gemme i buffer.
       //BufferedWriter bw = new BufferedWriter(fw);
       //lav Printobjekt
       PrintWriter output = new PrintWriter(fw);
-      output.println("Test af append");
+      output.println("Test af append - igen, igen!");
+      //tøm buffer til disk
       output.flush();
+      //ryd pænt op og luk PrintWriter-objektet
       output.close();
    }  
-   catch( IOException e ){
+   catch( FileNotFoundException e )
+   {
+      System.out.println("Fil ikke fundet: " + e);
+      
+   }
+   catch( IOException e )
+   {
       System.out.println("Fejl ved skrivning: " + e);
    }
   }
